ea8530f7f58751ff7562392fa95a86adf1a99589
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2021-01-19 08:34:55 -0300

    installed laravel/passport

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Laravel Api'),
 
     /*
     |--------------------------------------------------------------------------
@@ -175,6 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Laravel\Passport\PassportServiceProvider::class,
     ],
 
     /*
